7842a9614a7eec347d7a03859ba7f326f145d576
violet2901 <114224875+violet2901@users.noreply.github.com>
2023-07-06 13:24:05 +0700

    Add files via upload

---
+++ Fashion-Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fashion_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
