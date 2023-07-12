fd0ba80dc901673d0e8bf1744260979560b7ed34
violet2901 <114224875+violet2901@users.noreply.github.com>
2023-07-12 09:16:00 +0700

    asd

---
+++ Fashion_shop/Fashion_shop/Models/Bill_Details.cs
@@ -0,0 +1,9 @@
+﻿namespace Fashion_shop.Models
+{
+    public class Bill_Details
+    {
+        public int Bill_id { get; set; }
+        public int Item_id { get; set; }
+        public int Count { get; set; }
+    }
+}
