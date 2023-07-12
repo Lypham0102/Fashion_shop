3dad8f866b8dea00655c3936d9c182ebd689426d
Lypham0102 <100215514+Lypham0102@users.noreply.github.com>
2023-07-12 09:16:03 +0700

    Item

--- Fashion_shop/Fashion_shop/Models/Item.cs
+++ Fashion_shop/Fashion_shop/Models/Item.cs
@@ -2,5 +2,19 @@
 {
     public class Item
     {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public float Price { get; set; }
+        public string Gender { get; set; }
+        public int Count { get; set; }
+        public string Size { get; set; }
+        public string Country { get; set; }
+        public string Image { get; set; }
+        public int Materials_id { get; set; }
+        public int User_Item_Id { get; set; }
+        public int Product_Type_Id { get; set; }
+
     }
 }
