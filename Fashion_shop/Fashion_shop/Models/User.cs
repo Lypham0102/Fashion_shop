08720ae529284512137f7dcfceb638cce7fba82a
violet2901 <saeyoshino2901@gmail.com>
2023-07-16 15:27:49 +0700

    asd

---
+++ Fashion_shop/Fashion_shop/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fashion_shop.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set;}
+        public string Bank { get; set;}
+        public string CardNumber { get; set;}
+        public DateTime Date_of_birth { get; set;}
+        public int Role_id { get;}
+    }
+}
