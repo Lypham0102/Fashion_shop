dc0e0cdf3e24c3b345b285593672e424ab35a0e3
violet2901 <114224875+violet2901@users.noreply.github.com>
2023-07-24 13:34:20 +0700

    123

---
+++ Fashion_shop/Fashion_shop/Models/Item_list.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Fashion_shop.Models
+{
+    public class Item_list
+    {
+        public Item item;
+        public List<Color> colors;
+        public List<Size> sizes;
+
+        public Item_list(Item i, List<Color> c, List<Size> s)
+        {
+            item = i;
+            colors = c;
+            sizes = s;
+
+        }
+    }
+}
