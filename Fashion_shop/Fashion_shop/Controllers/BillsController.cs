96caef2106fa465e977f9e014282fd74aed5751d
Lypham0102 <100215514+Lypham0102@users.noreply.github.com>
2023-08-02 11:14:08 +0700

    add

--- Fashion_shop/Fashion_shop/Controllers/BillsController.cs
+++ Fashion_shop/Fashion_shop/Controllers/BillsController.cs
@@ -27,6 +27,11 @@ namespace Fashion_shop.Controllers
             return View(await _context.Bill.ToListAsync());
         }
 
+        public IActionResult IndexCus()
+        {
+            return View();
+        }
+
         // GET: Bills/Details/5
         public async Task<IActionResult> Details(int? id)
         {
