d53e8d5df8dbeb169b0aae88a824d939dbff5781
Lypham0102 <100215514+Lypham0102@users.noreply.github.com>
2023-08-03 03:55:29 +0700

    add

--- Fashion_shop/Fashion_shop/Controllers/HomeController.cs
+++ Fashion_shop/Fashion_shop/Controllers/HomeController.cs
@@ -7,18 +7,61 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Fashion_shop.Models;
 using Fashion_shop.Data;
+using System.Globalization;
+using System.Text;
 
 namespace Fashion_shop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _dbContext;
         //private AppDbContext dbContext;
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
+        [HttpPost]
+        public IActionResult Search(string keyword)
+        {
+            // Loại bỏ dấu khỏi từ khóa tìm kiếm
+            keyword = RemoveDiacritics(keyword.ToLower());
+
+            // Lấy danh sách tất cả các mục từ cơ sở dữ liệu
+            var allItems = _dbContext.Item.ToList();
+
+            // Truy vấn dữ liệu trong C# sau khi loại bỏ dấu
+            var result = allItems
+                .Where(item => RemoveDiacritics(item.Name.ToLower()).Contains(keyword))
+                .ToList();
+
+            // Trả về kết quả tìm kiếm dưới dạng danh sách sản phẩm và từ khóa tìm kiếm
+            ViewBag.Keyword = keyword;
+            return View("SearchResult", result);
+        }
+
+        // Hàm loại bỏ dấu
+        private static string RemoveDiacritics(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return string.Empty;
+
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
         public IActionResult Index()
         {
             var userName = Request.Cookies["UserName"];
